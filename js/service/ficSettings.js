320062e1d98a2c35e1aac00e7f2d855cc57e1e60
opensourceportfolio <leontager@yahoo.com>
2014-09-15 21:23:18 -0700

    Finished Parse integration

--- js/service/ficSettings.js
+++ js/service/ficSettings.js
@@ -3,14 +3,17 @@ services.factory('ficSettings', ['$q',
         'use strict';
 
         var defaults = {
-            'age': 28,
-            'networth': 100000,
-            'savings': 2000,
-            'withdrawal_rate': 4,
-            'goal': 50000,
-            'inflation': 3,
-            'ror': 8,
-        };
+                'age': 28,
+                'networth': 100000,
+                'savings': 2000,
+                'savings_rate': 1,
+                'withdrawal_rate': 4,
+                'goal': 50000,
+                'goal_rate': 2,
+                'inflation': 3,
+                'ror': 8,
+            },
+            cache = {};
 
         var FIData = Parse.Object.extend("FIData");
 
@@ -19,25 +22,38 @@ services.factory('ficSettings', ['$q',
             getSettings: function (userId) {
                 var query,
                     deferred = $q.defer(),
-                    settings;
+                    settings,
+                    o;
 
                 if (userId === 'anonymous') {
                     settings = JSON.parse(localStorage.getItem('fic-settings')) || {};
                     deferred.resolve(settings[userId] || defaults);
                 } else {
                     query = new Parse.Query(FIData);
-                    query.get(userId).then(function (current_settings) {
-                        deferred.resolve(current_settings || defaults);
-                    }, function () {
-                        deferred.resolve(defaults);
-                    });
+                    if (cache[userId]) {
+                        o = cache[userId];
+                        deferred.resolve(o.toJSON());
+                    } else {
+                        query.equalTo('userId', userId);
+                        query.find().then(function (current_settings) {
+                            o = current_settings[0]
+                            cache[userId] = o;
+                            deferred.resolve(o.toJSON() || defaults);
+                        }, function () {
+                            o = new FIData(defaults);
+                            o.set('userId', userId);
+                            cache[userId] = o;
+                            deferred.resolve(defaults);
+                        });
+                    }
                 }
 
                 return deferred.promise;
             },
 
             setSettings: function (userId, settings) {
-                var deferred = $q.defer();
+                var deferred = $q.defer(),
+                    o;
 
                 if (userId === 'anonymous') {
                     var current_settings = JSON.parse(localStorage.getItem('fic-settings')) || {};
@@ -47,8 +63,9 @@ services.factory('ficSettings', ['$q',
 
                     return deferred.promise;
                 } else {
-                    return SettingsService.getSettings(userId).then(function (current_settings) {
-                        return current_settings.save(settings);
+                    return SettingsService.getSettings(userId).then(function () {
+                        o = cache[userId];
+                        return o.save(settings);
                     });
                 }
             },
